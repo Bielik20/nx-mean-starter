d21ce4233a128f59c17389b610eb4b84e379ae44
“Damian Bielecki” <bielik20@gmail.com>
2018-11-12 22:52:04 +0100

    Import Server from original

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -2,17 +2,27 @@
  * This is not a production server yet!
  * This is only a minimal backend to get started.
  **/
-
+import { createMockPosts, Post } from '@nx-mean-starter/models';
 import * as express from 'express';
+import * as path from 'path';
+
+import { environment } from './environments/environment';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send(`Welcome to server!`);
+const posts: Post[] = createMockPosts();
+
+app.get('/api/posts', (req, res) => {
+  res.send(JSON.stringify(posts));
 });
 
+if (environment.production === true) {
+  // in production mode run application from dist folder
+  app.use(express.static(path.join(__dirname, '/../web-app')));
+}
+
 const port = 3333;
-app.listen(port, (err) => {
+app.listen(port, err => {
   if (err) {
     console.error(err);
   }
