f1629e6a814e1bdfea144e84a55920f50f12b806
Damian Bielecki <bielik20@gmail.com>
2018-12-24 20:26:43 +0100

    Fix AuthInterceptor spec

--- libs/state/auth/src/lib/interceptor/auth.interceptor.spec.ts
+++ libs/state/auth/src/lib/interceptor/auth.interceptor.spec.ts
@@ -1,11 +1,12 @@
 /* tslint:disable:no-unused-variable */
 import { inject, TestBed } from '@angular/core/testing';
+import { Store } from '@ngrx/store';
 import { AuthInterceptor } from './auth.interceptor';
 
 describe('Service: AuthInterceptor', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [AuthInterceptor],
+      providers: [AuthInterceptor, { provide: Store, useValue: { select: jest.fn() } }],
     });
   });
 
