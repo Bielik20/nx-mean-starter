cf4b2dbef024aeb1d9c4585eff167ea2c549563e
Damian Bielecki <bielik20@gmail.com>
2018-12-24 20:27:01 +0100

    Fix AuthService spec

--- libs/state/auth/src/lib/service/auth.service.spec.ts
+++ libs/state/auth/src/lib/service/auth.service.spec.ts
@@ -1,14 +1,19 @@
-import { inject, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
 import { AuthService } from './auth.service';
 
 describe('Service: Auth', () => {
+  let service: AuthService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [AuthService],
     });
+    service = TestBed.get(AuthService);
   });
 
-  it('should ...', inject([AuthService], (service: AuthService) => {
+  it('should ...', () => {
     expect(service).toBeTruthy();
-  }));
+  });
 });
