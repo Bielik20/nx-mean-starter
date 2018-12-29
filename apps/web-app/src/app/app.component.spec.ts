c182dd93e7270145b8dee34a488d7faf765482dd
Damian Bielecki <dbielecki@fandom.com>
2018-12-29 10:57:13 +0100

    Fix AppComponent spec

--- apps/web-app/src/app/app.component.spec.ts
+++ apps/web-app/src/app/app.component.spec.ts
@@ -1,7 +1,7 @@
 import { NO_ERRORS_SCHEMA } from '@angular/compiler/src/core';
 import { async, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { storeStubProvider } from '@nx-mean-starter/testing';
+import { StoreStub } from '@nx-mean-starter/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
@@ -9,7 +9,7 @@ describe('AppComponent', () => {
     TestBed.configureTestingModule({
       imports: [RouterTestingModule],
       declarations: [AppComponent],
-      providers: [storeStubProvider],
+      providers: [StoreStub.provide],
       schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
   }));
