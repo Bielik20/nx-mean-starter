960285f22065d9f2f3e1aa89cbb59fd317b5459c
Damian Bielecki <dbielecki@fandom.com>
2019-01-01 20:36:25 +0100

    Add AuthGuard spec

--- libs/feature/auth/src/lib/guard/auth.guard.spec.ts
+++ libs/feature/auth/src/lib/guard/auth.guard.spec.ts
@@ -1,15 +1,83 @@
-import { async, inject, TestBed } from '@angular/core/testing';
-
+import { TestBed } from '@angular/core/testing';
+import { MatDialog } from '@angular/material';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { AuthState } from '@nx-mean-starter/state/auth';
+import { RouterState } from '@nx-mean-starter/state/router';
+import { StoreStub } from '@nx-mean-starter/testing';
+import { Observable, of } from 'rxjs';
 import { AuthGuard } from './auth.guard';
 
 describe('AuthGuard', () => {
+  let guard: AuthGuard;
+  let store: StoreStub;
+  let beforeClosed: () => Observable<boolean>;
+
   beforeEach(() => {
+    const matDialogStub = {
+      open: jest.fn(() => ({ beforeClosed })),
+    };
+
     TestBed.configureTestingModule({
-      providers: [AuthGuard],
+      providers: [
+        AuthGuard,
+        StoreStub.provider,
+        { provide: MatDialog, useValue: matDialogStub },
+        { provide: Router, useValue: { navigate: jest.fn() } },
+      ],
+    });
+
+    guard = TestBed.get(AuthGuard);
+    store = TestBed.get(Store);
+  });
+
+  it('should ...', () => {
+    expect(guard).toBeTruthy();
+  });
+
+  it('should pass', done => {
+    store.overrideSelector(AuthState.getAuthenticated, true);
+    guard.canActivate(null, null).subscribe(pass => {
+      expect(pass).toBe(true);
+      done();
     });
   });
 
-  it('should ...', inject([AuthGuard], (guard: AuthGuard) => {
-    expect(guard).toBeTruthy();
-  }));
+  describe('first route', () => {
+    beforeEach(() => {
+      store.overrideSelector(AuthState.getAuthenticated, false);
+      store.overrideSelector(RouterState.getNavigationId, 1);
+    });
+
+    it('should fail with first route', done => {
+      const next: any = { url: [{ path: '/test-path', parameters: {} }] };
+      guard.canActivate(next, null).subscribe(pass => {
+        expect(pass).toBe(false);
+        done();
+      });
+    });
+  });
+
+  describe('modal', () => {
+    beforeEach(() => {
+      store.overrideSelector(AuthState.getAuthenticated, false);
+      store.overrideSelector(RouterState.getNavigationId, 2);
+    });
+
+    it('should fail', done => {
+      beforeClosed = () => of(false);
+      guard.canActivate(null, null).subscribe(pass => {
+        expect(pass).toBe(false);
+        done();
+      });
+    });
+
+    it('should pass', done => {
+      beforeClosed = () => of(true);
+      guard.canActivate(null, null).subscribe(pass => {
+        expect(pass).toBe(true);
+        done();
+      });
+    });
+  });
 });
