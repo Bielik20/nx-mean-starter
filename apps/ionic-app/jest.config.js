20a1d23203cd74ceb06cef1380dc010a9eef32bd
Damian Bielecki <bielik20@gmail.com>
2019-05-18 12:26:51 +0200

    Fix jest tests
    Sources:
    * https://github.com/ionic-team/ionic/issues/15695#issuecomment-490511950
    * https://stackoverflow.com/questions/51830523/jest-ionic-4-beta-import-platform-from-ionic-angular-gets-syntaxerror

--- apps/ionic-app/jest.config.js
+++ apps/ionic-app/jest.config.js
@@ -2,4 +2,7 @@ module.exports = {
   name: 'ionic-app',
   preset: '../../jest.config.js',
   coverageDirectory: '../../coverage/apps/ionic-app/',
+  transformIgnorePatterns: [
+    "node_modules/(?!@ionic-native|@ionic)"
+  ],
 };
