c0f6a00976040f1d5b4de5731818d22ff40e5100
Damian Bielecki <bielik20@gmail.com>
2019-06-10 18:50:16 +0200

    Add detect browser

--- apps/web-app/src/app/core/layout/pages/layout-page/layout-page.component.ts
+++ apps/web-app/src/app/core/layout/pages/layout-page/layout-page.component.ts
@@ -3,6 +3,7 @@ import { Store } from '@ngrx/store';
 import { routeAnimations } from '@nx-mean-starter/shared';
 import { LayoutState } from '@nx-mean-starter/state/layout';
 import { SettingsState } from '@nx-mean-starter/state/settings';
+import Browser from 'browser-detect';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -15,13 +16,30 @@ export class LayoutPageComponent implements OnInit {
   isMobile$: Observable<boolean>;
   showSidenav$: Observable<boolean>;
   theme$: Observable<SettingsState.ApplicationTheme>;
+  private browser = Browser();
 
-  constructor(private store: Store<LayoutState.State>) {}
+  constructor(private store: Store<LayoutState.State | SettingsState.State>) {}
 
   ngOnInit() {
     this.isMobile$ = this.store.select(LayoutState.getIsMobile);
     this.showSidenav$ = this.store.select(LayoutState.getShowSidenav);
     this.theme$ = this.store.select(SettingsState.getTheme);
+
+    this.setAnimations();
+  }
+
+  private setAnimations() {
+    if (this.isIEorEdgeOrSafari()) {
+      this.store.dispatch(
+        new SettingsState.ActionSettingsChangeAnimationsPageDisabled({
+          pageAnimationsDisabled: true,
+        }),
+      );
+    }
+  }
+
+  private isIEorEdgeOrSafari() {
+    return ['ie', 'edge', 'safari'].includes(this.browser.name);
   }
 
   toggleSidenav() {
