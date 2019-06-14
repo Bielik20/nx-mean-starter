e3ce8fe2e67284947cf3da0d53bb8f7dd1fff221
Damian Bielecki <bielik20@gmail.com>
2019-06-14 18:33:58 +0200

    Change linting style for testing

--- libs/testing/src/lib/stubs/router-link-directive.stub.ts
+++ libs/testing/src/lib/stubs/router-link-directive.stub.ts
@@ -3,15 +3,11 @@ import { Directive, Input } from '@angular/core';
 // export for convenience.
 export { RouterLink } from '@angular/router';
 
-/* tslint:disable:directive-class-suffix */
 @Directive({
-  // tslint:disable-next-line:directive-selector
   selector: '[routerLink]',
-  // tslint:disable-next-line:no-host-metadata-property
   host: { '(click)': 'onClick()' },
 })
 export class RouterLinkDirectiveStub {
-  // tslint:disable-next-line:no-input-rename
   @Input('routerLink') linkParams: any;
   navigatedTo: any = null;
 
