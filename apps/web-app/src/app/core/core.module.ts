bb7e09b95a66d7aa67f33431bc0c03ab40452ff3
Damian Bielecki <bielik20@gmail.com>
2019-06-16 09:52:13 +0200

    Improve responsive card layout

--- apps/web-app/src/app/core/core.module.ts
+++ apps/web-app/src/app/core/core.module.ts
@@ -3,13 +3,12 @@ import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FeatureAuthModule } from '@nx-mean-starter/feature/auth';
 import { SharedModule } from '@nx-mean-starter/shared';
-import {
-  FooterComponent,
-  LayoutComponent,
-  NavbarComponent,
-  UserSidenavCardComponent,
-} from './layout/components';
-import { LayoutPageComponent, SidenavPageComponent } from './layout/pages';
+import { FooterComponent } from './components/footer/footer.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { UserSidenavCardComponent } from './components/user-sidenav-card/user-sidenav-card.component';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { SidenavPageComponent } from './pages/sidenav-page/sidenav-page.component';
 
 @NgModule({
   imports: [CommonModule, SharedModule, FeatureAuthModule, RouterModule],
